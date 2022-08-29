2581295c465b8afd9aaa48d52eb96a8dc27ac6a3
Vladislav Ryzhikh <vladislavryzhikh@gmail.com>
2022-08-29 16:26:33 +0100

    Added new tests of Game class

--- test/test_Chess_classes.py
+++ test/test_Chess_classes.py
@@ -2,20 +2,29 @@ import pytest
 from Chess_classes import Board, Piece, Rook, Knight, Bishop, Queen, King, Pawn, Game
 
 
-@pytest.fixture
-def my_game():
-    """
-    returns a Game instance
-    """
-    return Game()
+class TestGame:
+    @pytest.fixture
+    def my_game(self):
+        """
+        returns a Game instance
+        """
+        return Game()
 
+    @pytest.mark.parametrize("notation,expected", [
+        ('e4', (3, 4)),
+        ('h8', (7, 7)),
+        ('b9', False),
+        ('k3', False)
+    ])
+    def test_notation_to_coordinates(self, my_game, notation, expected):
+        assert my_game.notation_to_coordinates(notation) == expected
 
-@pytest.mark.parametrize("notation,expected", [
-    ('e4', (3, 4)),
-    ('h8', (7, 7)),
-    ('b9', False),
-    ('k3', False)
-])
-def test_notation_to_coordinates(my_game, notation, expected):
-    assert my_game.notation_to_coordinates(notation) == expected
+    def test_increment_move(self, my_game):
+        my_game.increment_move()
+        assert my_game.move_number == 2
 
+    def test_change_turn(self, my_game):
+        my_game.change_turn()
+        assert my_game.turn == 'B'
+        my_game.change_turn()
+        assert my_game.turn == 'W'
