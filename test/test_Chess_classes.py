3cc40b110c2cd5a782314c6127e719804312b6da
Vladislav Ryzhikh <vladislavryzhikh@gmail.com>
2022-08-29 16:02:07 +0100

    added the first of a Game method

--- test/test_Chess_classes.py
+++ test/test_Chess_classes.py
@@ -1,3 +1,21 @@
 import pytest
+from Chess_classes import Board, Piece, Rook, Knight, Bishop, Queen, King, Pawn, Game
 
-print('hello world')
+
+@pytest.fixture
+def my_game():
+    """
+    returns a Game instance
+    """
+    return Game()
+
+
+@pytest.mark.parametrize("notation,expected", [
+    ('e4', (3, 4)),
+    ('h8', (7, 7)),
+    ('b9', False),
+    ('k3', False)
+])
+def test_notation_to_coordinates(my_game, notation, expected):
+    assert my_game.notation_to_coordinates(notation) == expected
+
