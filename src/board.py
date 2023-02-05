efdf926ac4bc73e6523a18f5364120a939d49291
vldryz <vladislavryzhikh@gmail.com>
2023-02-05 18:51:36 +0000

    feat [WIP]: `Board.__init__` increased readability.

--- src/board.py
+++ src/board.py
@@ -8,4 +8,54 @@ from utils import Colour
 
 
 class Board:
-    ...
+    """Class used to manage the state of the chess board."""
+    def __init__(self):
+        self.state: list[list[Rook | Knight | Bishop | King | Queen | Pawn | None]] = [
+            [
+                Rook(Colour.WHITE),
+                Knight(Colour.WHITE),
+                Bishop(Colour.WHITE),
+                Queen(Colour.WHITE),
+                King(Colour.WHITE),
+                Bishop(Colour.WHITE),
+                Knight(Colour.WHITE),
+                Rook(Colour.WHITE),
+            ],
+            [Pawn(Colour.WHITE) for _ in range(8)],
+            [None] * 8,
+            [None] * 8,
+            [None] * 8,
+            [None] * 8,
+            [Pawn(Colour.BLACK) for _ in range(8)],
+            [
+                Rook(Colour.BLACK),
+                Knight(Colour.BLACK),
+                Bishop(Colour.BLACK),
+                Queen(Colour.BLACK),
+                King(Colour.BLACK),
+                Bishop(Colour.BLACK),
+                Knight(Colour.BLACK),
+                Rook(Colour.BLACK),
+            ],
+        ]
+
+    def make_move(self, start, end):
+        ...
+
+    def __str__(self):
+        return (
+            (
+                (
+                    "\n"
+                    + "\n".join(
+                        "".join(f"{piece} " if piece else ". " for piece in row)
+                        + " "
+                        + str(8 - col)
+                        for col, row in enumerate(self.state[::-1])
+                    )
+                )
+                + "\n\n"
+            )
+            + " ".join(["a", "b", "c", "d", "e", "f", "g", "h"])
+            + "\n"
+        )
