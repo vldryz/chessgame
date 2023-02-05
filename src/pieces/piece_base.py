abf6bfd9126f807088f406e5a8126ec5c6c028ca
vldryz <vladislavryzhikh@gmail.com>
2023-02-05 18:49:07 +0000

    feat [WIP]: `Piece` base abstract class to manage pieces

--- src/pieces/piece_base.py
+++ src/pieces/piece_base.py
@@ -1,10 +1,31 @@
-"""This module provides a base class for chess pieces."""
+"""This module provides a base abstract class for chess pieces."""
 # ——————————————————————————————————————————— Imports ——————————————————————————————————————————— #
+# Standard libraries
+from abc import ABC, abstractmethod
+
 # Dependencies
 from utils import Colour
 
 # ———————————————————————————————————————————— Code ———————————————————————————————————————————— #
 
 
-class Piece:
-    ...
+class Piece(ABC):
+    """Base class for chess pieces."""
+    def __init__(self, colour, icon):
+        self.colour: Colour = colour
+        self.icon: str = icon
+        self.moved: bool = False
+
+    def __str__(self):
+        return self.icon
+
+    @abstractmethod
+    def available_moves(self) -> list[tuple[int, int]]:
+        """Returns a list of available moves for the piece.
+
+        Returns:
+            list[tuple[int, int]]: A list of available moves for the piece.
+
+        """
+        raise NotImplementedError()
+
--- src/pieces/pieces.py
+++ src/pieces/pieces.py
@@ -8,24 +8,54 @@ from utils import Colour
 
 
 class Pawn(Piece):
-    ...
+    def __init__(self, colour: Colour):
+        super().__init__(colour, '♟' if colour == Colour.WHITE else '♙')
 
+        # Pawn-specific attributes
+        self.en_passant: bool = False
 
-class Knight(Piece):
-    ...
-
-
-class Bishop(Piece):
-    ...
-
-
-class Rook(Piece):
-    ...
-
-
-class Queen(Piece):
-    ...
+    def available_moves(self) -> list[tuple[int, int]]:
+        ...
 
 
 class King(Piece):
-    ...
+    def __init__(self, colour: Colour):
+        super().__init__(colour, '♚' if colour == Colour.WHITE else '♔')
+
+        # King-specific attributes
+        self.checked: bool = False
+
+    def available_moves(self) -> list[tuple[int, int]]:
+        ...
+
+
+class Knight(Piece):
+    def __init__(self, colour: Colour):
+        super().__init__(colour, '♞' if colour == Colour.WHITE else '♘')
+
+    def available_moves(self) -> list[tuple[int, int]]:
+        ...
+
+
+class Bishop(Piece):
+    def __init__(self, colour: Colour):
+        super().__init__(colour, '♝' if colour == Colour.WHITE else '♗')
+
+    def available_moves(self) -> list[tuple[int, int]]:
+        ...
+
+
+class Rook(Piece):
+    def __init__(self, colour: Colour):
+        super().__init__(colour, '♜' if colour == Colour.WHITE else '♖')
+
+    def available_moves(self) -> list[tuple[int, int]]:
+        ...
+
+
+class Queen(Piece):
+    def __init__(self, colour: Colour):
+        super().__init__(colour, '♛' if colour == Colour.WHITE else '♕')
+
+    def available_moves(self) -> list[tuple[int, int]]:
+        ...
