26381f1613560b2352af3d3a7bf326cca9c61216
vldryz <vladislavryzhikh@gmail.com>
2023-02-16 15:23:42 +0000

    feat: `possible_moves` for all pieces

--- src/chess/pieces/piece_base.py
+++ src/chess/pieces/piece_base.py
@@ -19,40 +19,39 @@ class Piece(ABC):
     def __str__(self):
         return self.icon
 
+    # @abstractmethod
+    # def legal_moves(self, start) -> list[tuple[int, int]]:
+    #     """The method to get a list of legal moves for a piece.
+    #
+    #     Legal moves are defined as a subset of available moves
+    #     that do not put the king in check.
+    #
+    #     Note:
+    #         Checking for an ability to castle is not necessary,
+    #         as the ability to castle implies that a king and a rook
+    #         have legal moves.
+    #
+    #     Args:
+    #         start (tuple[int, int]): The starting position of the piece.
+    #         board (Board): The board on which the piece is located.
+    #
+    #     Returns:
+    #         list[tuple[int, int]]: A list of legal moves for the piece.
+    #
+    #     """
+    #
+    #     raise NotImplementedError()
+
     @abstractmethod
-    def legal_moves(self, start) -> list[tuple[int, int]]:
-        """The method to get a list of legal moves for a piece.
-
-        Legal moves are defined as a subset of available moves
-        that do not put the king in check.
-
-        Note:
-            Checking for an ability to castle is not necessary,
-            as the ability to castle implies that a king and a rook
-            have legal moves.
-
-        Args:
-            start (tuple[int, int]): The starting position of the piece.
-            board (Board): The board on which the piece is located.
-
-        Returns:
-            list[tuple[int, int]]: A list of legal moves for the piece.
-
-        """
-
-        raise NotImplementedError()
-
-    @abstractmethod
-    def _available_moves(self, start) -> list[tuple[int, int]]:
+    def possible_moves(self, start: tuple[int, int]) -> list[tuple[int, int]]:
         """The method to get a list of available moves for a piece.
 
-        Available moves are defined as moves to which a piece can
-        technically move, regardless of whether it will put the king in check.
-        Or in other words, the moves that are not blocked by other pieces.
+        Possible moves are defined as moves to which a piece can
+        technically move, given the starting position, regardless
+        of the position of other pieces.
 
         Args:
             start (tuple[int, int]): The starting position of the piece.
-            board (Board): The board on which the piece is located.
 
         Returns:
             list[tuple[int, int]]: A list of available moves for the piece.
--- src/chess/pieces/piece_classes.py
+++ src/chess/pieces/piece_classes.py
@@ -1,79 +1,111 @@
 """This module provides classes for chess pieces."""
 # ——————————————————————————————————————————— Imports ——————————————————————————————————————————— #
+# Standard libraries
+from itertools import product
+
 # Dependencies
 from .piece_base import Piece
 from ..colours import Colour
+
 
 # ———————————————————————————————————————————— Code ———————————————————————————————————————————— #
 
 
 class Pawn(Piece):
     def __init__(self, colour: Colour):
-        super().__init__(colour, '♟' if colour == Colour.WHITE else '♙')
+        super().__init__(colour, "♟" if colour == Colour.WHITE else "♙")
 
         # Pawn-specific attributes
         self.en_passant: bool = False
 
-    def legal_moves(self, start) -> list[tuple[int, int]]:
-        ...
+    def possible_moves(self, start: tuple[int, int]) -> list[tuple[int, int]]:
+        moves = [(start[0], start[1] + 1)]
 
-    def _available_moves(self, start) -> list[tuple[int, int]]:
-        ...
+        if start[0] != 0:
+            moves.append((start[0] - 1, start[1] + 1))
+
+        if start[0] != 7:
+            moves.append((start[0] + 1, start[1] + 1))
+
+        if not self.moved:
+            moves.append((start[0], start[1] + 2))
+
+        return moves
 
 
 class King(Piece):
     def __init__(self, colour: Colour):
-        super().__init__(colour, '♚' if colour == Colour.WHITE else '♔')
+        super().__init__(colour, "♚" if colour == Colour.WHITE else "♔")
 
         # King-specific attributes
         self.checked: bool = False
 
-    def legal_moves(self, start) -> list[tuple[int, int]]:
-        ...
+    def possible_moves(self, start: tuple[int, int]) -> list[tuple[int, int]]:
+        moves = [
+            (start[0] + file, start[1] + rank)
+            for file, rank in product(range(-1, 2), range(-1, 2))
+            if not file == rank == 0
+        ]
 
-    def _available_moves(self, start) -> list[tuple[int, int]]:
-        ...
+        return list(filter(lambda move: 0 <= move[0] <= 7 and 0 <= move[1] <= 7, moves))
 
 
 class Knight(Piece):
     def __init__(self, colour: Colour):
-        super().__init__(colour, '♞' if colour == Colour.WHITE else '♘')
+        super().__init__(colour, "♞" if colour == Colour.WHITE else "♘")
 
-    def legal_moves(self, start) -> list[tuple[int, int]]:
-        ...
+    def possible_moves(self, start: tuple[int, int]) -> list[tuple[int, int]]:
+        moves = [
+            (start[0] + file, start[1] + rank)
+            for i, j in product((-1, 1), (-2, 2))
+            for file, rank in {(i, j), (j, i)}
+        ]
 
-    def _available_moves(self, start) -> list[tuple[int, int]]:
-        ...
+        return list(filter(lambda move: 0 <= move[0] <= 7 and 0 <= move[1] <= 7, moves))
 
 
 class Bishop(Piece):
     def __init__(self, colour: Colour):
-        super().__init__(colour, '♝' if colour == Colour.WHITE else '♗')
+        super().__init__(colour, "♝" if colour == Colour.WHITE else "♗")
 
-    def legal_moves(self, start) -> list[tuple[int, int]]:
-        ...
+    def possible_moves(self, start: tuple[int, int]) -> list[tuple[int, int]]:
+        moves = [
+            (start[0] + file, start[1] + rank)
+            for i, j in zip(range(1, 8), range(-1, -8, -1))
+            for file, rank in {(i, j), (j, i), (-i, j), (i, -j)}
+        ]
 
-    def _available_moves(self, start) -> list[tuple[int, int]]:
-        ...
+        return list(filter(lambda move: 0 <= move[0] <= 7 and 0 <= move[1] <= 7, moves))
 
 
 class Rook(Piece):
     def __init__(self, colour: Colour):
-        super().__init__(colour, '♜' if colour == Colour.WHITE else '♖')
+        super().__init__(colour, "♜" if colour == Colour.WHITE else "♖")
 
-    def legal_moves(self, start) -> list[tuple[int, int]]:
-        ...
-
-    def _available_moves(self, start) -> list[tuple[int, int]]:
-        ...
+    def possible_moves(self, start: tuple[int, int]) -> list[tuple[int, int]]:
+        return [(start[0], rank) for rank in range(8) if rank != start[0]] + [
+            (file, start[1]) for file in range(8) if file != start[1]
+        ]
 
 
 class Queen(Piece):
     def __init__(self, colour: Colour):
-        super().__init__(colour, '♛' if colour == Colour.WHITE else '♕')
+        super().__init__(colour, "♛" if colour == Colour.WHITE else "♕")
 
-    def legal_moves(self, start) -> list[tuple[int, int]]:
-        ...
+    def possible_moves(self, start: tuple[int, int]) -> list[tuple[int, int]]:
+        moves = [
+            (start[0] + file, start[1] + rank)
+            for i, j in zip(range(1, 8), range(-1, -8, -1))
+            for file, rank in {(i, j), (j, i), (-i, j), (i, -j)}
+        ]
 
-    def _available_moves(self, start) -> list[tuple[int, int]]:
-        ...
+        moves = list(
+            filter(lambda move: 0 <= move[0] <= 7 and 0 <= move[1] <= 7, moves)
+        )
+
+        moves.extend(
+            [(start[0], rank) for rank in range(8) if rank != start[0]]
+            + [(file, start[1]) for file in range(8) if file != start[1]]
+        )
+
+        return moves
