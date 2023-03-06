129f37384b8a39025f7ec1c0eb323d6b55859c2f
vldryz <vladislavryzhikh@gmail.com>
2023-03-06 22:48:31 +0000

    chore: removed `src` folder

--- tests/test_pieces/test_piece_classes.py
+++ tests/test_pieces/test_piece_classes.py
@@ -1,11 +1,8 @@
 """This module provides classes for chess pieces."""
 # ——————————————————————————————————————————— Imports ——————————————————————————————————————————— #
 # 3rd party libraries
-import pytest
 
 # Dependencies
-from chess.pieces import Pawn, King, Knight, Bishop, Rook, Queen
-from chess.colours import Colour
 
 
 # ———————————————————————————————————————————— Tests ———————————————————————————————————————————— #
